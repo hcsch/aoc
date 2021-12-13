b08591bad173bcf2fe1709adfa4f9a011d019215
Hans Christian Schmitz <hcsch@hcsch.eu>
2021-12-13 02:20:46 +0100

    Use an enum instead of `&str`s

--- aoc-solutions/src/year2021/day12.rs
+++ aoc-solutions/src/year2021/day12.rs
@@ -1,31 +1,68 @@
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 // Algorithm adapted from
 // https://github.com/petgraph/petgraph/blob/9ff688872b467d3e1b5adef19f5c52f519d3279c/src/algo/simple_paths.rs#L36-L102
 
-struct UnGraph<'a> {
-    adjacency_map: HashMap<&'a str, Vec<&'a str>>,
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum Cave {
+    Start,
+    End,
+    Small(u16),
+    Large(u16),
 }
 
-impl<'a> UnGraph<'a> {
+impl FromStr for Cave {
+    type Err = &'static str;
+
+    fn from_str(cave_str: &str) -> Result<Self, Self::Err> {
+        match cave_str {
+            "start" => Ok(Self::Start),
+            "end" => Ok(Self::End),
+            small_cave if small_cave.bytes().all(|b| b.is_ascii_lowercase()) => Ok(Self::Small(
+                small_cave
+                    .bytes()
+                    .fold(0, |acc, b| acc * 26 + (b - b'a') as u16),
+            )),
+            large_cave if large_cave.bytes().all(|b| b.is_ascii_uppercase()) => Ok(Self::Large(
+                large_cave
+                    .bytes()
+                    .fold(0, |acc, b| acc * 26 + (b - b'A') as u16),
+            )),
+            _ => Err("expected `start`, `end`, a lowercase cave name, an uppercase cave name"),
+        }
+    }
+}
+
+struct Graph {
+    adjacency_map: HashMap<Cave, HashSet<Cave>>,
+}
+
+impl Graph {
     pub fn new() -> Self {
-        UnGraph {
+        Graph {
             adjacency_map: HashMap::new(),
         }
     }
 
-    pub fn add_edge(&mut self, node_a: &'a str, node_b: &'a str) {
-        self.adjacency_map
-            .entry(node_a)
-            .or_insert(Vec::new())
-            .push(node_b);
-        self.adjacency_map
-            .entry(node_b)
-            .or_insert(Vec::new())
-            .push(node_a);
+    pub fn add_edge(&mut self, node_a: Cave, node_b: Cave) {
+        if node_a != Cave::End && node_b != Cave::Start {
+            self.adjacency_map
+                .entry(node_a)
+                .or_insert(HashSet::new())
+                .insert(node_b);
+        }
+        if node_b != Cave::End && node_a != Cave::Start {
+            self.adjacency_map
+                .entry(node_b)
+                .or_insert(HashSet::new())
+                .insert(node_a);
+        }
     }
 
-    pub fn from_edges<I: Iterator<Item = (&'a str, &'a str)>>(iterator: I) -> Self {
+    pub fn from_edges<I: Iterator<Item = (Cave, Cave)>>(iterator: I) -> Self {
         let mut graph = Self::new();
         for (node_a, node_b) in iterator {
             graph.add_edge(node_a, node_b);
@@ -33,35 +70,36 @@ impl<'a> UnGraph<'a> {
         graph
     }
 
-    pub fn neighbors(&'a self, node: &str) -> impl 'a + Iterator<Item = &'a str> {
+    pub fn neighbors<'a>(&'a self, node: Cave) -> impl 'a + Iterator<Item = Cave> {
         self.adjacency_map
-            .get(node)
-            .map(|neighbors| neighbors as &[&'a str])
-            .unwrap_or(&[])
+            .get(&node)
+            .map(|neighbors| neighbors.iter().copied())
             .into_iter()
-            .copied()
+            .flatten()
     }
 }
 
-fn parse_input<'a>(input_lines: &'a Vec<String>) -> UnGraph<'a> {
-    UnGraph::from_edges(input_lines.iter().map(|line| line.split_once('-').unwrap()))
+fn parse_input<I: Iterator<Item = String>>(input_lines: I) -> Graph {
+    Graph::from_edges(input_lines.map(|line| {
+        let (cave_a_str, cave_b_str) = line.split_once('-').unwrap();
+        (cave_a_str.parse().unwrap(), cave_b_str.parse().unwrap())
+    }))
 }
 
 pub fn solve_puzzle1<I: Iterator<Item = String>>(input_lines: I) -> String {
-    let input_lines = input_lines.collect();
-    let cave_graph = parse_input(&input_lines);
+    let cave_graph = parse_input(input_lines);
 
     let mut num_paths: usize = 0;
 
-    let mut children_stack = vec![cave_graph.neighbors("start")];
-    let mut visited = vec!["start"];
+    let mut children_stack = vec![cave_graph.neighbors(Cave::Start)];
+    let mut visited = vec![Cave::Start];
 
     while let Some(children) = children_stack.last_mut() {
         if let Some(child) = children.next() {
-            if child == "end" {
+            if child == Cave::End {
                 num_paths += 1;
                 continue;
-            } else if child.bytes().all(|b| b.is_ascii_uppercase()) {
+            } else if let Cave::Large(_) = child {
                 visited.push(child);
                 children_stack.push(cave_graph.neighbors(child));
             } else if !visited.contains(&child) {
@@ -83,23 +121,22 @@ pub fn solve_puzzle1<I: Iterator<Item = String>>(input_lines: I) -> String {
 }
 
 pub fn solve_puzzle2<I: Iterator<Item = String>>(input_lines: I) -> String {
-    let input_lines = input_lines.collect();
-    let cave_graph = parse_input(&input_lines);
+    let cave_graph = parse_input(input_lines);
 
     let mut num_paths: usize = 0;
 
-    let mut children_stack = vec![cave_graph.neighbors("start")];
-    let mut path_stack = vec!["start"];
-    let mut visited = vec!["start"];
+    let mut children_stack = vec![cave_graph.neighbors(Cave::Start)];
+    let mut path_stack = vec![Cave::Start];
+    let mut visited = vec![Cave::Start];
 
     let mut small_cave_visited_twice = None;
 
     while let Some(children) = children_stack.last_mut() {
         if let Some(child) = children.next() {
-            if child == "end" {
+            if child == Cave::End {
                 num_paths += 1;
                 continue;
-            } else if child.bytes().all(|b| b.is_ascii_uppercase()) {
+            } else if let Cave::Large(_) = child {
                 visited.push(child);
                 path_stack.push(child);
                 children_stack.push(cave_graph.neighbors(child));
@@ -107,7 +144,7 @@ pub fn solve_puzzle2<I: Iterator<Item = String>>(input_lines: I) -> String {
                 visited.push(child);
                 path_stack.push(child);
                 children_stack.push(cave_graph.neighbors(child));
-            } else if small_cave_visited_twice.is_none() && child != "start" {
+            } else if small_cave_visited_twice.is_none() && child != Cave::Start {
                 visited.push(child);
                 path_stack.push(child);
                 children_stack.push(cave_graph.neighbors(child));
