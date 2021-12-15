376426d4603bf21d87c0af5e5f77c05ce8b736ba
Hans Christian Schmitz <hcsch@hcsch.eu>
2021-12-15 22:49:56 +0100

    Compute tiled map values lazily
    This cuts the memory cost about in half for part 2.

--- aoc-solutions/src/year2021/day15.rs
+++ aoc-solutions/src/year2021/day15.rs
@@ -5,6 +5,9 @@ use array_init;
 const TILE_WIDTH: usize = 100;
 const TILE_HEIGHT: usize = 100;
 
+const X_TILES: usize = 5;
+const Y_TILES: usize = 5;
+
 fn parse_input<I: Iterator<Item = String>>(input_lines: I) -> [[u8; TILE_WIDTH]; TILE_HEIGHT] {
     let risk_levels = array_init::from_iter(input_lines.map(|line| {
         array_init::from_iter(line.into_bytes().into_iter().map(|b| b - b'0')).unwrap()
@@ -14,13 +17,41 @@ fn parse_input<I: Iterator<Item = String>>(input_lines: I) -> [[u8; TILE_WIDTH];
     risk_levels
 }
 
+trait Map {
+    fn get(&self, index: [u16; 2]) -> u8;
+}
+
+struct TiledMap {
+    base_tile: [[u8; TILE_WIDTH]; TILE_HEIGHT],
+}
+
+impl Map for TiledMap {
+    fn get(&self, index: [u16; 2]) -> u8 {
+        let (base_x, base_y) = (
+            (index[1] as usize % TILE_WIDTH),
+            (index[0] as usize % TILE_HEIGHT),
+        );
+        let (tile_x, tile_y) = (
+            (index[1] / TILE_WIDTH as u16) as u8,
+            (index[0] / TILE_HEIGHT as u16) as u8,
+        );
+        (self.base_tile[base_y][base_x] - 1 + (tile_x + tile_y) as u8) % 9 + 1
+    }
+}
+
+impl<const TILE_WIDTH: usize, const TILE_HEIGHT: usize> Map for [[u8; TILE_WIDTH]; TILE_HEIGHT] {
+    fn get(&self, index: [u16; 2]) -> u8 {
+        self[index[0] as usize][index[1] as usize]
+    }
+}
+
 // Algorithm adapted from
 // https://github.com/rust-lang/rust/blob/d594910a2da12f158477b4c7281716f535cfa3de/library/alloc/src/collections/binary_heap.rs#L20-L95
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 struct DijkstraState {
     risk: u16,
-    position: [usize; 2],
+    position: [u16; 2],
 }
 
 impl Ord for DijkstraState {
@@ -39,7 +70,7 @@ impl PartialOrd for DijkstraState {
     }
 }
 
-fn neighbors(map_dim: [usize; 2], position: [usize; 2]) -> impl Iterator<Item = [usize; 2]> {
+fn neighbors(map_dim: [usize; 2], position: [u16; 2]) -> impl Iterator<Item = [u16; 2]> {
     [
         // above
         position[0].checked_sub(1).map(|y| [y, position[1]]),
@@ -48,29 +79,27 @@ fn neighbors(map_dim: [usize; 2], position: [usize; 2]) -> impl Iterator<Item = 
         // below
         position[0]
             .checked_add(1)
-            .filter(|&y| y < map_dim[0])
+            .filter(|&y| y < map_dim[0] as u16)
             .map(|y| [y, position[1]]),
         // right
         position[1]
             .checked_add(1)
-            .filter(|&x| x < map_dim[1])
+            .filter(|&x| x < map_dim[1] as u16)
             .map(|x| [position[0], x]),
     ]
     .into_iter()
     .filter_map(|neighboring_pos| neighboring_pos)
 }
 
-fn dijkstra_min_risk<const MAP_WIDTH: usize, const MAP_HEIGHT: usize>(
-    risk_map: [[u8; MAP_WIDTH]; MAP_HEIGHT],
-) -> u16 {
-    const START: [usize; 2] = [0, 0];
-    let end = [MAP_HEIGHT - 1, MAP_WIDTH - 1];
+fn dijkstra_min_risk<M: Map, const MAP_WIDTH: usize, const MAP_HEIGHT: usize>(risk_map: M) -> u16 {
+    const START: [u16; 2] = [0, 0];
+    let end = [(MAP_HEIGHT - 1) as u16, (MAP_WIDTH - 1) as u16];
 
     let mut risk_of_path_to = [[u16::MAX; MAP_WIDTH]; MAP_HEIGHT];
 
     let mut heap = BinaryHeap::new();
 
-    risk_of_path_to[START[0]][START[1]] = 0;
+    risk_of_path_to[START[0] as usize][START[1] as usize] = 0;
     heap.push(DijkstraState {
         risk: 0,
         position: START,
@@ -81,19 +110,19 @@ fn dijkstra_min_risk<const MAP_WIDTH: usize, const MAP_HEIGHT: usize>(
             return risk;
         }
 
-        if risk > risk_of_path_to[position[0]][position[1]] {
+        if risk > risk_of_path_to[position[0] as usize][position[1] as usize] {
             continue;
         }
 
         for neighboring_pos in neighbors([MAP_HEIGHT, MAP_WIDTH], position) {
             let next = DijkstraState {
-                risk: risk + risk_map[neighboring_pos[0]][neighboring_pos[1]] as u16,
+                risk: risk + risk_map.get(neighboring_pos) as u16,
                 position: neighboring_pos,
             };
 
-            if next.risk < risk_of_path_to[next.position[0]][next.position[1]] {
+            if next.risk < risk_of_path_to[next.position[0] as usize][next.position[1] as usize] {
                 heap.push(next);
-                risk_of_path_to[next.position[0]][next.position[1]] = next.risk;
+                risk_of_path_to[next.position[0] as usize][next.position[1] as usize] = next.risk;
             }
         }
     }
@@ -104,7 +133,7 @@ fn dijkstra_min_risk<const MAP_WIDTH: usize, const MAP_HEIGHT: usize>(
 pub fn solve_puzzle1<I: Iterator<Item = String>>(input_lines: I) -> String {
     let risk_map = parse_input(input_lines);
 
-    let solution = dijkstra_min_risk(risk_map);
+    let solution = dijkstra_min_risk::<_, TILE_WIDTH, TILE_HEIGHT>(risk_map);
 
     solution.to_string()
 }
@@ -112,23 +141,10 @@ pub fn solve_puzzle1<I: Iterator<Item = String>>(input_lines: I) -> String {
 pub fn solve_puzzle2<I: Iterator<Item = String>>(input_lines: I) -> String {
     let risk_map_tile = parse_input(input_lines);
 
-    // Concatenate mapped tile into 5 tile row.
-    let mut risk_map = [[0; TILE_WIDTH * 5]; TILE_HEIGHT * 5];
-    for tile_y in 0..5 {
-        for (row_i, row) in risk_map_tile.iter().enumerate() {
-            for tile_x in 0..5 {
-                for (dst, src) in risk_map[tile_y * TILE_HEIGHT + row_i]
-                    [tile_x * TILE_WIDTH..(tile_x + 1) * TILE_WIDTH]
-                    .iter_mut()
-                    .zip(row)
-                {
-                    *dst = (src - 1 + (tile_x as u8 + tile_y as u8)) % 9 + 1;
-                }
-            }
-        }
-    }
-
-    let solution = dijkstra_min_risk(risk_map);
+    let solution =
+        dijkstra_min_risk::<_, { X_TILES * TILE_WIDTH }, { Y_TILES * TILE_HEIGHT }>(TiledMap {
+            base_tile: risk_map_tile,
+        });
 
     solution.to_string()
 }
